bb0988f55d3de5ee435e2e9df48b870b6faab9b8
zachary mines <zmines@wesleyan.edu>
2020-06-20 17:43:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect{|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+  i = 0
+  j = 1
+  new_array = []
+  while i < array.size
+    new_array << "Hello, #{array[i]}! You'll be assigned to room #{j}!"
+    i +=1
+    j +=1
+  end
+  new_array
+end
+
+def printer(array)
+  batch_badge_creator(array).each {|name| puts name}
+  assign_rooms(array).each {|name| puts name}
+end
