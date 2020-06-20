4ac284667a08d99df61d87e347b5103d9df2478f
zachary mines <zmines@wesleyan.edu>
2020-06-20 17:37:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,10 +3,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.collect{|name| badge_maker(name)
+  array.collect{|name| badge_maker(name)}
 end
 
 def assign_rooms(array)
   i = 0
-  array.collect
+  
+  array.collect{|name| "Hello, #{name}, "
 end
